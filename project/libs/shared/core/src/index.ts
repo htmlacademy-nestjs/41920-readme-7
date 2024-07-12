0e519d6865571de985e83ac8e4b3171acf58244d
Sherstnev Alexey <aleksey.sherstnev@raiffeisen.ru>
2024-07-13 00:57:53 +0200

    add started entities

---
+++ project/libs/shared/core/src/index.ts
@@ -0,0 +1,4 @@
+export { Entity } from './lib/base/entity';
+
+export { StorableEntity } from './lib/interfaces/storable-entity.interface';
+export { EntityFactory } from './lib/interfaces/entity-factory.interface';
